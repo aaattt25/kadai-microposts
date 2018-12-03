7ab9cee511e5d288d91fbbc041e4c38b5a78c55d
aaa.ttt <p.aaattt25@gmail.com>
2018-12-03 01:41:34 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -4,12 +4,7 @@
         
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             
-        </button>
-        
-        <div class="collapse navbar-collapse" id="nav-bar">
-            <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav">
-                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+        </button>'signup.get', 'Signup' ,[],['class'=>'nav-link']) !!}</li>
                 <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
             </ul>
         </div>
