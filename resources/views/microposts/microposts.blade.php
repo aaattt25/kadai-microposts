fcc519ab4044238d234b5632c2e3c9002fd9fb12
aaa.ttt <p.aaattt25@gmail.com>
2018-12-20 11:33:30 +0000

    adjust butoon-space

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -10,10 +10,10 @@
                     <p>{!! nl2br(e($micropost->content)) !!}</p>
                 </div>
                 <div class="row">
-                    <div class="col">
+                    <div class="col-sm-2">
                         @include('favorite.favorite_button', ['micropost' => $micropost])
                     </div>
-                    <div class="col">
+                    <div class="col-sm-2">
                         @if (Auth::id() == $micropost->user_id)
                             {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                                 {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
