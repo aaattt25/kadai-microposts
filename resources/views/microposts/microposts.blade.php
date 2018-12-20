c272d3078f03c1d9f9602185c6248c7934992cee
aaa.ttt <p.aaattt25@gmail.com>
2018-12-20 11:58:10 +0000

    adjust button-space2

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -10,10 +10,10 @@
                     <p>{!! nl2br(e($micropost->content)) !!}</p>
                 </div>
                 <div class="row">
-                    <div class="col-sm-2">
+                    <div class="col-4">
                         @include('favorite.favorite_button', ['micropost' => $micropost])
                     </div>
-                    <div class="col-sm-2">
+                    <div class="col-4">
                         @if (Auth::id() == $micropost->user_id)
                             {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                                 {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
